14979b2ac135142fd578640a36665e08873b888a
unknown <nicolerspader@gmail.com>
2024-03-05 12:01:27 -0300

    basico

---
+++ exercicios/index.js
@@ -0,0 +1,28 @@
+// comando para exibir mensagens
+console.log("Hello World")
+
+//exercicio de calculos
+
+const num1 = 1900
+const num2 = 9
+
+//soma
+let soma = num1 + num2
+
+console.log("valor da soma é: " + soma);
+
+//------------------------------------------------------------------
+
+
+//subtração
+let subtraçao = num1 - num2
+
+console.log("valor da subtraçao é: " + subtraçao);
+
+//------------------------------------------------------------------
+
+
+//divisão
+let divisao = num1/num2
+
+console.log("valor da subtraçao é: " + divisao);
