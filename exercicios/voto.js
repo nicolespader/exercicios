f22aa6af594e4c8b5fa37ced5eccb1ada0f84de7
unknown <nicolerspader@gmail.com>
2024-03-08 11:44:05 -0300

    exercicio votação

---
+++ exercicios/voto.js
@@ -0,0 +1,9 @@
+const roberto = 70;
+
+if(roberto < 16){
+    console.log("Não vota");
+} else if(roberto >= 16 && roberto < 18||roberto >= 70){
+    console.log("Facultativo");
+} else if(roberto >= 18 && roberto < 70){
+    console.log("Obrigatório");
+}
